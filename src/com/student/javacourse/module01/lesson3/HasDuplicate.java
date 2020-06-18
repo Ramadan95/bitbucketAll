7b3842076f076314e347dcebf563e2ba96b0ac28
Ramadan95 <kagermanov-r@mail.ru>
2020-06-18 21:51:08 +0300

    changed name the class SearchDuplicate to HasDuplicate

--- src/com/student/javacourse/module01/lesson3/HasDuplicate.java
+++ src/com/student/javacourse/module01/lesson3/HasDuplicate.java
@@ -2,13 +2,13 @@ package com.student.javacourse.module01.lesson3;
 
 import java.util.Arrays;
 
-public class SearchDuplicate {
+public class HasDuplicate {
     public static void main(String[] args) {
         int[] arr = {6,2,3,4,5,6};
-        System.out.println(arrDuplicate(arr));
+        System.out.println(hasDuplicate(arr));
     }
 
-    static boolean arrDuplicate(int[] arr) {
+    static boolean hasDuplicate(int[] arr) {
         Arrays.sort(arr);
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == arr[i + 1]) return true;
